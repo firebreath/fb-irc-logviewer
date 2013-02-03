f03bddec763bb640030bd1b58f6676204bec8b14
Keith McClelland <keith@hopster.com>
2013-02-03 07:33:05 -0600

    LogEntry.what no longer duplicates who and when.

--- app/models/log_entry.rb
+++ app/models/log_entry.rb
@@ -7,13 +7,16 @@ class LogEntry < ActiveRecord::Base
     return unless log_file && line
     line.chomp!
     begin
-      if line =~ /\[([\d\:]+)\] <(.*?)>/
+      regexp = /\[([\d\:]+)\] <(.*?)> /
+      if line =~ regexp
         line_time = $1
         line_who  = $2
+        line_what = line.gsub(regexp, '')
 
         file_date = log_file.file_date.strftime('%Y%m%d')
         log_when = Time.strptime("#{file_date} #{line_time}", "%Y%m%d %H:%M:%S")
-        log_file.log_entries.where(:when => log_when, :who => line_who, :what => line).first_or_create!
+
+        log_file.log_entries.where(:when => log_when, :who => line_who, :what => line_what).first_or_create!
       end
     rescue Exception => e
       puts "#{log_file.file_name}: unable to load: #{line}\nmessage:#{e.to_s}"
